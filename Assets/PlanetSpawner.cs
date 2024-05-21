a0a93d850120f7adec0a0bf5044e4cbbee54a019
Bobfree546 <amoshung9@gmail.com>
2024-05-21 23:30:29 +0800

    Update Planer Spawner
    GetNextOrbitPlanetPosition
    CheckAndDestroyPlanet

--- Assets/PlanetSpawner.cs
+++ Assets/PlanetSpawner.cs
@@ -1,41 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class PlanetSpawner : MonoBehaviour
 {
+    private const float PLANET_SHIFT = 5.0f;
+
     public GameObject planetGameObject;
     public static int planetCount = 0;
-    private int nextY;
+    private Queue<GameObject> planets;
 
     // Start is called before the first frame update
     void Start()
     {
-        nextY = 5;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        while (planetCount < 3)
-        {
-            SpawnPlanet();
-        }
+
     }
 
     void SpawnPlanet()
     {
-        int nextX = Random.Range(-10, 10);
-
-        planetCount++;
+        float nextX = Random.Range(-5, 5);
+        float nextY = planets.Last().transform.position.y + PLANET_SHIFT;
 
         Vector3 newPosition = new Vector3(nextX, nextY, 0);
         GameObject planetObject = Instantiate(planetGameObject, newPosition, Quaternion.identity);
-        planetObject.GetComponent<Planet>().planetId = planetCount;
-        planetObject.GetComponent<Planet>().rotateSpeed = Random.Range(-20,20);
+        planetObject.GetComponent<Planet>().rotateSpeed = Random.Range(-20, 20);
 
-        nextY += 5;
+        planets.Enqueue(planetObject);
+    }
+
+    Vector3 GetNextOrbitPlanetPosition(Vector3 RocketPosition, Vector3 CurrentOrbitPlanetPosition)
+    {
+        return planets
+            .Where(p => p.transform.position.y > RocketPosition.y && !p.transform.position.Equals(CurrentOrbitPlanetPosition))
+            .First().transform.position;
+    }
+
+    void CheckAndDestroyPlanet(float cameraLowestY)
+    {
+        while (planets.First().transform.position.y < (cameraLowestY - Planet.LARGEST_PLANET_RADIUS))
+        {
+            GameObject.Destroy(planets.Dequeue());
+        }
     }
 }
